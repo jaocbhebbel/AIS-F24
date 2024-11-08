698ef448d3aeb5212b0f9abdba1c3e1890c12241
jacob528 <jacoblleonart528@gmail.com>
2024-11-08 16:58:57 -0500

    CNN Started

---
+++ Chess/chessmodel.py
@@ -0,0 +1,73 @@
+import random
+import chess
+import chess.engine
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+class ThunderByteCNN(nn.Module):
+    #Initialize the Convolutional Neural Network
+    def __init__(self):
+        #Calls the parent function (my CNN model) with the parameters and abilities of the nn.Module from pytorch
+        super(ThunderByteCNN, self).__init__()
+
+        #Create convolutional layers
+        self.convlayer1 = nn.Conv2d(14, 32, kernel_size=3, padding=1)
+        self.convlayer2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.convlayer3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+
+
+    #def forward(self):
+        
+#Function to generate a random board for neural network to analyze
+def generate_board(board, depth):
+    if (depth == 0): 
+        return board
+    else:
+        move = random.choice(list(board.legal_moves))
+        board.push(move)
+        print()
+        print("Move:", move)
+        print(board)
+        generate_board(board, depth - 1)
+
+#Determine the Material on both sides 
+def calculate_material(board):
+    # Define the piece values
+    piece_values = {
+        chess.PAWN: 1,
+        chess.KNIGHT: 3,
+        chess.BISHOP: 3,
+        chess.ROOK: 5,
+        chess.QUEEN: 9
+    }
+
+    # Initialize the material score
+    white_material = 0
+    black_material = 0
+
+    # Iterate over all pieces on the board
+    for square, piece in board.piece_map().items():
+        # Get the value of the piece type (e.g., PAWN, KNIGHT, etc.)
+        piece_value = piece_values.get(piece.piece_type, 0)
+
+        # Add to the appropriate color's total
+        if piece.color == chess.WHITE:
+            white_material += piece_value
+        else:
+            black_material += piece_value
+
+    return white_material, black_material
+
+#Main Function
+if __name__ == "__main__":
+    #Define Board, depth and model
+    board = chess.Board()
+    maxdepth = 18
+    model = ChessCNN()
+
+    generate_board(board, maxdepth)
+    white_material, black_material = calculate_material(board)
+    print(white_material)
+    print(black_material)

8cdd8ec150a2088e5138bebf9629e02eb2c11bda
jameswu05 <145163478+jameswu05@users.noreply.github.com>
2024-11-12 17:33:55 -0500

    started UI for loading images

--- Chess/chessmodel.py
+++ Chess/chessmodel.py
@@ -7,6 +7,35 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
 
+# define piece images
+PIECE_IMAGES = {
+    'p': 'black_pawn.png',
+    'n': 'black_knight.png',
+    'b': 'black_bishop.png',
+    'r': 'black_rook.png',
+    'q': 'black_queen.png',
+    'k': 'black_king.png'
+    'P': 'white_pawn.png',
+    'N': 'white_knight.png',
+    'B': 'white_bishop.png',
+    'R': 'white_rook.png',
+    'Q': 'white_queen.png',
+    'K': 'white_king.png',
+}
+
+# initialize pygame
+pygame.init()
+
+# set up display dimensions
+WIDTH, HEIGHT = 512, 512
+SQUARE_SIZE  = WIDTH // 8
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("ThunderByte Chess")
+
+# colors
+LIGHT_SQUARE = (240, 217, 181)
+DARK_SQUARE = (181, 136, 99)
+
 class ThunderByteCNN(nn.Module):
     #Initialize the Convolutional Neural Network
     def __init__(self):
@@ -61,12 +90,21 @@ def calculate_material(board):
 
     return white_material, black_material
 
+# load piece images
+def load_images():
+    pieces = ['p', 'n', 'b', 'r', 'q', 'k', 'P', 'N', 'B', 'R', 'Q', 'K']
+    for piece in pieces:
+        image = pygame.image.load(f'assets/pawn.png')
+        image = pygame.transform.scale(image, (SQUARE_SIZE, SQUARE_SIZE))
+        PIECE_IMAGES[piece] = image
+
 #Main Function
 if __name__ == "__main__":
     #Define Board, depth and model
     board = chess.Board()
     maxdepth = 18
-    model = ChessCNN()
+    model = ThunderByteCNN()
+    load_images()
 
     generate_board(board, maxdepth)
     white_material, black_material = calculate_material(board)

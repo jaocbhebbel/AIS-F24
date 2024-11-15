0a1a3a9e4ce25394a19acfcb02be501b29da2f37
jaocbhebbel <jacob.hebbel@gmail.com>
2024-11-15 16:30:35 -0500

    to try and avoid commit chaos

--- Othello/code/nn.py
+++ Othello/code/nn.py
@@ -1,18 +1,1 @@
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.distributions import Categorical
-
-class PolicyNetwork(nn.Module):
-    def __init__(self):
-        super(PolicyNetwork, self).__init__()
-        self.fc = nn.Sequential(
-            nn.Linear(4, 128),
-            nn.ReLU(),
-            nn.Linear(128, 2),
-            nn.Softmax(dim=-1),
-        )
-
-    def forward(self, x):
-        return self.fc(x)
+## our neural network
